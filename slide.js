32d2b35076b042248b9fe653359686ab551ab8ec
Matthew Heidemann <matthew.heidemann@gmail.com>
2013-10-16 08:11:30 -0600

    Initial commit

---
+++ slide.js
@@ -0,0 +1,32 @@
+function main() {
+	var amount = 0;
+
+	function error() {
+	  	throw "Blars"
+	}
+	$('#error').on('click', error)
+
+
+	function nodeRemoval() {
+		$(this).remove()
+	}
+	$('#node-rem').on('click', nodeRemoval)
+
+	function subModification() {
+		$(this).append('s')
+	}
+	$('#sub-tree').on('click', subModification)
+
+	function attrModification() {
+		$(this).addClass('red')
+	}
+	$('#attr-mod').on('click', attrModification)
+
+	function condIncrement() {
+		amount += 1
+	}
+	$('#cond-button').on('click', condIncrement)
+}
+
+
+main()
